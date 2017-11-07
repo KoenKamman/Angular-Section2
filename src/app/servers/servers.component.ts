bcf0125d76e4a25b5ac7e081ef0b42d9be6c92d6
Koen Kamman <kammankoen@gmail.com>
2017-11-07 20:25:25 +0100

    Section 2-20

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-servers',
@@ -6,7 +6,6 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
     <app-server></app-server>
     <app-server></app-server>`,
   styleUrls: ['./servers.component.css'],
-  encapsulation: ViewEncapsulation.None
 })
 export class ServersComponent implements OnInit {
 
